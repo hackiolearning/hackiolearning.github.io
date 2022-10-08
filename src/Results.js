08dde48ba7e598d218caa6a85b227e81625feb43
Luke Trenaman <trenamanluke@gmail.com>
2022-10-08 17:53:09 -0400

    Description comment

--- src/Results.js
+++ src/Results.js
@@ -2,6 +2,13 @@ import React from "react";
 
 function Results(props){
     let data = props.data;
+    /*
+        If project type is web development, learning style is course, and level is high level, suggest:
+        * HTML, CSS, Javascript
+
+        Coursera course
+        * https://www.udemy.com/course/the-web-developer-bootcamp/
+    */
     return <div>
         <p>For your form, you inputted the following things</p>
         <p>Do you have experience? {data.hasExperience}</p>
