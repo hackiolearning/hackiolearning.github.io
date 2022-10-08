091a8ec6492ec99ad2891c36418fafa4866ed736
Luke Trenaman <trenamanluke@gmail.com>
2022-10-08 15:11:25 -0400

    frontend prototype

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import './css/style.css';
 import Form from './Form.js';
 
 class App extends React.Component{
+  continue(){
+    let state = this.state;
+    console.log(state);
+    state["questionNumber"]++;
+    this.setState(state);
+  }
+  back(){
+    let state = this.state;
+    console.log(state);
+    state["questionNumber"]--;
+    this.setState(state);
+  }
   handleChange(property,value) {
     let state = this.state;
     state[property] = value;
-    state["questionNumber"]++;
     this.setState(state);
-    console.log(this.state);
+    console.log(state);
   }
   constructor(){
     super();
     this.handleChange = this.handleChange.bind(this);
+    this.continue = this.continue.bind(this);
+    this.back = this.back.bind(this);
     this.state = {
       "questionNumber":0,
       "hasExperience":undefined,
@@ -24,7 +37,7 @@ class App extends React.Component{
   render(){
     return <>
     <p>Form should appear below this</p>
-      <Form data={this.state} handleChange={this.handleChange}/>
+      <Form data={this.state} handleChange={this.handleChange} continue={this.continue} back={this.back}/>
     </>
   }
 }
--- src/Form.js
+++ src/Form.js
@@ -9,58 +9,52 @@ class Answer{
 class Question extends React.Component{
     constructor(props){
         super();
-        this.state = {
-            "toggles": new Array(props.answers.length).fill(false)
-        };
-    }
-    onChange(idx){
-        if(this.props.formType === "radio"){
-            return function(){
-                let state = this.state;
-                state.toggles = new Array(this.props.answers.length).fill(false);
-                state.toggles[idx] = true;
-                this.setState(state);
-            }
-        }
-        if(this.props.formType === "multiselect"){
-            return function(){
-                let state = this.state;
-                state.toggles[idx] = !state.toggles[idx];
-                this.setState(state);
-            }
-        }
     }
     
-    doValues(){
-        let property = this.props.property;
-        let value;
+    onChange(idx){
+
         if(this.props.formType === "multiselect"){
-            value = [];
-            for(let i = 0;i<this.state.toggles.length;i++){
-                if(this.state.toggles[i]){
-                    value.push(this.props.answers[i].val);
+            return () => {
+                let property = this.props.property;
+                let val = this.props.answers[idx].val;
+                let arr = this.props.data[property];
+                let searchIndex = arr.indexOf(val);
+                if(searchIndex === -1){
+                    arr.push(val);
+                } else{
+                    arr.splice(searchIndex);
                 }
+                this.props.handleChange(property,arr);
             }
         }
         if(this.props.formType === "radio"){
-            value = this.props.answers[this.props.answers.length-1].val;
-            for(let i = 0;i<this.state.toggles.length;i++){
-                if(this.state.toggles[i]){
-                    value = this.props.answers[i].val;
-                }
+            return () => {
+                let property = this.props.property;
+                let value = this.props.answers[idx].val;
+                this.props.handleChange(property,value);
             }
         }
-        this.props.handleChange(property,value);
     }
     render(){
+        let appProp = this.props.data[this.props.property];
         const answerElems = this.props.answers.map((answer,idx) => {
-            return <button onClick={this.onChange(idx).bind(this)} key={idx} className={"button"  + (this.state.toggles[idx] ? " is-primary" : "")}>{answer.text}</button>
+            let shouldBeSelected = false;
+            if(this.props.formType === "radio"){
+                shouldBeSelected = appProp===answer.val;
+            }
+            if(this.props.formType === "multiselect"){
+                shouldBeSelected = appProp.indexOf(answer.val) !== -1;
+            }
+            return <button onClick={this.onChange(idx).bind(this)} key={idx} className={"button"  + (shouldBeSelected ? " is-primary" : "")}>{answer.text}</button>
         });
         return (<div className="form">
             <p className="title">{this.props.question}</p>
             {answerElems}
             <div className="control">
-                <button className="button is-primary" onClick={this.doValues.bind(this)}>Continue</button>
+                <button className="button is-primary" onClick={this.props.back}>Back</button>
+            </div>
+            <div className="control">
+                <button className="button is-primary" onClick={this.props.continue}>Continue</button>
             </div>
         </div>);
     }
@@ -75,7 +69,7 @@ function Form(props) {
     formState = <Question question="Have you coded before?"formType="radio" property="hasExperience" answers={[
         new Answer("yes",true),
         new Answer("no",false)
-    ]} handleChange={props.handleChange}/>
+    ]} handleChange={props.handleChange} continue={props.continue} data={data} back={props.back}/>
   }
   if(data.questionNumber === 1){
     formState = <Question question="What software do you want to make?" formType="multiselect" property="projectInterest" answers={[
@@ -84,22 +78,25 @@ function Form(props) {
         new Answer("Servers","server"),
         new Answer("Data visualizations","data-visualization"),
         new Answer("Data structures and algorithms","data-structures-algorithms")
-    ]} handleChange={props.handleChange}/>
+    ]} handleChange={props.handleChange} continue={props.continue} data={data} back={props.back}/>
   }
   if(data.questionNumber === 2){
-    formState = <Question question="How do you learn best?"formType="multiselect" property="learningType" answers={[
+    formState = <Question question="How do you learn best?" formType="multiselect" property="learningType" answers={[
         new Answer("Hands-on","hands-on"),
         new Answer("Courses","courses"),
         new Answer("Reading","reading"),
         new Answer("Video","video")
-    ]} handleChange={props.handleChange}/>
+    ]} handleChange={props.handleChange} continue={props.continue} data={data} back={props.back}/>
   }
   if(data.questionNumber === 3){
     formState = <Question question="What type of language do you want to learn?" formType="multiselect" property="level" answers={[
         new Answer("Block coding","block-coding"),
         new Answer("High-level","high-level"),
         new Answer("Low-level","low-level")
-    ]} handleChange={props.handleChange}/>
+    ]} handleChange={props.handleChange} continue={props.continue} data={data} back={props.back}/>
+  }
+  if(data.questionNumber === 4){
+    formState = <p>You have completed the form</p>
   }
   return (
     <div className="box">
