6db52664afb8ed82b69be8eb0ef6e2eb1c7921d9
Luke Trenaman <trenamanluke@gmail.com>
2022-10-08 20:32:08 -0400

    Code before removing continue button

---
+++ src/Homepage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Homepage(props){
+    return <section className="hero is-fullheight">
+    <div className="hero-body">
+      <div className="box homepage-box is-flex is-align-items-center is-flex-direction-column container is-max-desktop">
+        <p className="title is-1 has-text-centered">
+          Welcome!
+        </p>
+        <p className="subtitle has-text-centered">
+        Square One is a hub of computer science learning sources designed to help you effectively learn new languages, get used to new services, and get started on new projects
+        </p>
+        <button className="button is-large is-danger" onClick={props.continue}>get started</button>
+      </div>
+    </div>
+  </section>
+}
+export default Homepage;
