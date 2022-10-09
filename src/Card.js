c2eca0c2efab645b5ced060a18792bbfdacdc868
Luke Trenaman <trenamanluke@gmail.com>
2022-10-08 23:10:18 -0400

    column responsiveness

--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Card(props){
     let name = "box has-background-" + props.color;
-    return <div className="column is-one-third m-0 " >
+    return <div className="column is-one-third-desktop is-half-tablet m-0 " >
         <div className={name}>
-            <p className="subtitle is-2">{props.name}</p>
+            <p className="subtitle is-3">{props.name}</p>
             {props.desc}
         </div>
     </div>
